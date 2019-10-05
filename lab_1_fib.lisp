fc0362832517ca4fa0d8156d091694c3a76470c1
W014ara <bezruk2000@mail.ru>
2019-10-05 10:44:57 +0300

    lab1_ver2

--- lab_1_fib.lisp
+++ lab_1_fib.lisp
@@ -3,11 +3,12 @@
 		1
 		(* n (factorial (- n 1)))))
 
-(defun fib(n)
-	if (< n 3)
-	1
-	(+ (fib(- n 1)) fib(- n 2)))
+(defun fibonacci(n)
+    (if (< n 3) 
+    1
+    (+ (fibonacci(- n 1)) (fibonacci(- n 2)))))
+
 
 (loop for i from 1 to 16
-	do (format t "~D," (fib i))
-	finally (format t ",,~%"))
+    do (format t "~D," (fibonacci i))
+    finally (format t ",,~%"))
