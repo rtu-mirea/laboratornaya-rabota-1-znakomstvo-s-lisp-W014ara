f168711d6e5499433ec773c9404ba18a494ba140
W014ara <bezruk2000@mail.ru>
2019-10-05 10:30:28 +0300

    lab_1_factorial_fib

---
+++ lab_1_fib.lisp
@@ -0,0 +1,13 @@
+(defun factorial(n)
+	(if (= n 0)
+		1
+		(* n (factorial (- n 1)))))
+
+(defun fib(n)
+	if (< n 3)
+	1
+	(+ (fib(- n 1)) fib(- n 2)))
+
+(loop for i from 1 to 16
+	do (format t "~D," (fib i))
+	finally (format t ",,~%"))
